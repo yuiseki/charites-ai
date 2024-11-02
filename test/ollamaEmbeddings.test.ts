6afd544c12658d8464205750122618830c3ccfbf
yuiseki <yuiseki@gmail.com>
2024-11-02 12:28:29 +0900

    Add ollamaEmbeddings test

---
+++ test/ollamaEmbeddings.test.ts
@@ -0,0 +1,41 @@
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import type { Document } from "@langchain/core/documents";
+import { expect, test } from "vitest";
+
+test("ollama embedding", async () => {
+  const embeddings = new OllamaEmbeddings({
+    baseUrl: "http://localhost:11434",
+    model: "all-minilm:l6-v2",
+  });
+
+  const vectorStore = new MemoryVectorStore(embeddings);
+
+  const document1: Document = {
+    pageContent: "The powerhouse of the cell is the mitochondria",
+    metadata: { source: "https://example.com" },
+  };
+
+  const document2: Document = {
+    pageContent: "Buildings are made out of brick",
+    metadata: { source: "https://example.com" },
+  };
+
+  const document3: Document = {
+    pageContent: "Mitochondria are made out of lipids",
+    metadata: { source: "https://example.com" },
+  };
+
+  const documents = [document1, document2, document3];
+
+  await vectorStore.addDocuments(documents);
+
+  const similaritySearchResults = await vectorStore.similaritySearch(
+    "biology",
+    1
+  );
+
+  expect(similaritySearchResults[0].pageContent).toBe(
+    "The powerhouse of the cell is the mitochondria"
+  );
+});
