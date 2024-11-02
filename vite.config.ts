494eba339120fd24209362d764dc644d31da4ffa
yuiseki <yuiseki@gmail.com>
2024-11-02 17:47:22 +0900

    langchainjs-ollama-testerでやることにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/charites-ai/",
-  test: {
-    testTimeout: 10 * 60 * 1000, // 10 minutes
-  },
 });
